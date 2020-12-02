7f5946b2e61cdf2bd3ef2e7241765118858a6286
Andy Agus <Redagus@gmail.com>
2020-12-02 12:09:49 -0500

    added square and horse

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Knight from './Knight'
-
-ReactDOM.render(<Knight />, document.getElementById('root'));
+import Knight from './components/Knight'
+import Square from './components/Square'
+import Board from './components/Board'
+ReactDOM.render(
+		<Board knightPosition={[0,0]} />,
+		document.getElementById('root')
+	);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
